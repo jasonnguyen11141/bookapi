8cd7c2b9913ac1b25825416f7eef2eb62212a311
jasonnguyen11141 <nguy1442@msu.edu>
2025-03-17 01:48:15 +0000

    changes

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uyyoxzfkzuhbinzwfkss.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5eW94emZrenVoYmluendma3NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNDY5MjcsImV4cCI6MjA1NzcyMjkyN30.e8Xka7CzlNNN_Ajtx-qaW0PN_8x0CP9Ghpt_xM6nR08'
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
-
 async function getBooks(){
   let { data: books, error } = await supabase
     .from('books')
@@ -13,5 +12,4 @@ async function getBooks(){
     bookList.innerHTML += `<li>${book.title} - ${book.author} - ISBN: ${book.ISBN}</li>`
   }
 } 
-
 getBooks()
